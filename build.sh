b20d9d59019bd3fae290e4f6e4ce8c0d4117b51e
Jerome Boisvert-Chouinard <jerome.boisvertchouinard@savoirfairelinux.com>
2017-03-22 12:32:16 -0400

    improve build script

--- build.sh
+++ build.sh
@@ -2,5 +2,6 @@ if [ ! -d build ]
 then
     mkdir build
 fi
-gcc -Wall -std=c11 -g mpc.c -g jblisp.c -g repl.c -o build/jblisp -leditline -lm
-gcc -Wall -std=c11 -g mpc.c -g jblisp.c -g tests/test.c -o build/test -lm
+gcc -Wall -std=c11 -c -g mpc.c -g jblisp.c -g repl.c -g tests/test.c
+gcc -o build/jblisp mpc.o jblisp.o repl.o -lm -leditline
+gcc -o build/test mpc.o jblisp.o test.o -lm
