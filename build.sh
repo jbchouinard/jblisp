1c0f1b8aeefa1bf07409958044877c9909570af5
Jerome Boisvert-Chouinard <jerome.boisvertchouinard@savoirfairelinux.com>
2017-03-21 21:58:39 -0400

    use c11 standard

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@ if [ ! -d build ]
 then
     mkdir build
 fi
-gcc -Wall -std=c99 -g mpc.c -g repl.c -o build/jblisp -leditline -lm
+gcc -Wall -std=c11 -g mpc.c -g repl.c -o build/jblisp -leditline -lm
