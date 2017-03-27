ada197dfe0d1fabaf25789c5fa80e9a8eddbdeb2
Jerome Boisvert-Chouinard <jerome.boisvertchouinard@savoirfairelinux.com>
2017-03-27 15:11:15 -0400

    add exit and stop options

--- repl.c
+++ repl.c
@@ -14,13 +14,26 @@ int main(int argc, char **argv) {
     // Load language
     exec_file(env, "lang/base.jbl");
 
+    int run_repl=1;
+    int argp;
+    // Process CLI switches
+    for (argp=1; argp < argc; argp++) {
+        if (argv[argp][0] == '-' && argv[argp][1] == '-') {
+            if (strcmp(argv[argp], "--stop") == 0) {
+                run_repl=0;
+            }
+        }
+        else { break; }
+    }
+
     // Load CLI-specified files
-    for (int i=1; i < argc; i++) {
+    for (int i=argp; i < argc; i++) {
         exec_file(env, argv[i]);
     }
 
-    while (1) {
+    while (run_repl) {
         char *input = readline("jblisp> ");
+        if (strcmp(input, "(exit)")==0) { free(input); break; }
         add_history(input);
         exec_line(env, input);
         free(input);
--- test.sh
+++ test.sh
@@ -5,4 +5,4 @@ echo ""
 echo ""
 echo "RUNNING LISP TESTS..."
 echo ""
-./build/jblisp tests/test.jbl
+./build/jblisp --stop tests/test.jbl
