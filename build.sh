a87bc8f13a7ad7f837f32798ecfb01a3fdb8d2a5
Jerome Boisvert-Chouinard <jerome.boisvertchouinard@savoirfairelinux.com>
2017-06-10 00:27:20 -0400

    update build dir

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ then
     mkdir build
 fi
 gcc -Wall -std=c11 -c -g mpc.c -g jblisp.c -g repl.c -g tests/test.c
-gcc -o build/jblisp mpc.o jblisp.o repl.o -lm -leditline
-gcc -o build/test mpc.o jblisp.o test.o -lm
+gcc -o bin/jblisp mpc.o jblisp.o repl.o -lm -leditline
+gcc -o bin/test mpc.o jblisp.o test.o -lm
